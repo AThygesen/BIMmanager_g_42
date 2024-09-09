989d007955730d4e9c8feda41048add64b3e3712
AThygesen <augustthy@gmail.com>
2024-09-09 18:02:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import ifcopenshell
 
-from .external.BIManalyst_g_xy.rules import windowRule
-from .external.BIManalyst_g_xy.rules import doorRule
+from .external.BIManalyst_g_40.rules import windowRule
+from .external.BIManalyst_g_44.rules import doorRule
 
 model = ifcopenshell.open("path/to/ifcfile.ifc")
 
