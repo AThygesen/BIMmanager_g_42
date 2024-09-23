0d0b09172d5daee74c35aa2da0b9993bd662b389
AThygesen <augustthy@gmail.com>
2024-09-23 14:41:41 +0200

    updated group 41

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import ifcopenshell
 
-from .external.BIManalyst_g_41.rules import Qty_Of_Structural_Elements
-from .external.BIManalyst_g_44 import
-
+from .external.BIManalyst_g_40_new import main
+#from .external.BIManalyst_g_41.rules import Qty_Of_Structural_Elements
+#from .external.BIManalyst_g_44.rules import beam_price_based_on_meter
+    
 
 model = ifcopenshell.open("C:/Users/August Thygesen/OneDrive/DTU/7. semester/Advanced BIM\ModelsLLYN - ARK.ifc")
 
-windowRule = windowRule.checkRule(model)
+beam_rule = main(model)
 #Qty_of_structural = Qty_Of_Structural_Elements.checkRule(model)
 #beam_price = beam_price_based_on_meter.checkRule(model)
 
-print("Window result:", windowRule)
+print("Beam rule:", beam_rule)
 #print("Qty of structural elements:", Qty_of_structural)
 #print("Beam price:", beam_price)
-
-
