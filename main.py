58859e94488f71b98bf52bf2ff2a80224815ac06
AThygesen <augustthy@gmail.com>
2024-09-23 16:02:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import ifcopenshell
 from external.BIManalyst_g_41.rules import BikeSpace
 #from .external.BIManalyst_g_44.rules import beam_price_based_on_meter
 
-model = ifcopenshell.open("C:/Users/August Thygesen/OneDrive/DTU/7. semester/Advanced BIM/Models/LLYN - ARK.ifc")
-
+model_arc = ifcopenshell.open("C:/Users/August Thygesen/OneDrive/DTU/7. semester/Advanced BIM/Models/CES_BLD_24_06_ARC.ifc")
+model_str = ifcopenshell.open("C:/Users/August Thygesen/OneDrive/DTU/7. semester/Advanced BIM/Models/CES_BLD_24_06_STR.ifc")
 
 #beam_rule = beamCheck(model)
-Bikespace_result = BikeSpace.count_furniture_shelving_storage(model)
+Bikespace_result = BikeSpace.count_furniture_shelving_storage(model_arc)
 #beam_price = beam_price_based_on_meter.checkRule(model)
 
 #print("Beam rule:", beam_rule)
