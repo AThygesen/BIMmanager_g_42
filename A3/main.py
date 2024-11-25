bf8b8ab46d50890f88fbd172a58767817e2550e7
LilMouses <130827065+LilMouses@users.noreply.github.com>
2024-11-25 13:20:20 +0100

    Update main.py

--- A3/main.py
+++ A3/main.py
@@ -1,1 +1,19 @@
-# this should be the main script that pulls data from IFC file and compares to Cost database
+from database import window_to_then_wall
+import ifcopenshell
+import pandas as pd
+
+# This code processes IFC building model data to extract information about doors, walls, and windows,
+# combining it with cost data from a database to calculate quantities and costs.
+# It creates a DataFrame with details like element type, quantity, unit price, and total cost,
+# and exports the results to an Excel file. The main function also maps element categories and quantities
+# to cost data for detailed analysis.
+# The example database is in this case an Excel file
+
+# Load the IFC file
+ifc_file = ifcopenshell.open("PLACEHOLDER")
+database = 'eksempel_database.xlsx'
+
+cost_df = pd.read_excel(database, usecols='B, D', skiprows=2)
+# runs script
+window_to_then_wall(ifc_file, cost_df)
+
